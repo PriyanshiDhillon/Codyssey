d91bed7a36b25af4e57f2e7dabb0c888cce4993c
PriyanshiDhillon <146756002+PriyanshiDhillon@users.noreply.github.com>
2024-03-11 15:35:25 +0530

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,13 @@
+int celebrity(vector<vector<int>>& M, int n) {
+int indegree[n] = { 0 }, outdegree[n] = { 0 };
+for (int i = 0; i < n; i++) {
+for (int j = 0; j < n; j++) {
+int x = M[i][j];
+outdegree[i] += x;
+indegree[j] += x;
+}
+}
+for (int i = 0; i < n; i++)
+if (indegree[i] == n - 1 && outdegree[i] == 0) return i;
+return -1;
+}
